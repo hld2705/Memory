16c85996c9503ec8ac9999bb5ce3b2de3a193c7d
Halid Crnkic <halidcrnkic@gmail.com>
2026-06-17 19:31:01 +0200

    added player .svg-s to the db.ts also the draw signs

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,6 +4,9 @@ export const themes = {
     gaming_theme: {
         menu_picture: "./assets/cards/gamingtheme/gamingmenu.png",
         backside: "./public/assets/cards/gamingtheme/backface/gamingbackfacecard.png",
+        player_blue: "./assets/cards/gamingtheme/winner_pockal_gaming_theme.svg",
+        player_orange: "./assets/cards/gamingtheme/winner_pockal_gaming_theme.svg",
+        draw_scale: "./assets/cards/gamingtheme/scale_icon_gaming_theme.svg",
         frontside_path: "./public/assets/cards/gamingtheme/frontface/",
         frontside: [
             "gamingacecardfrontface.png",
@@ -29,6 +32,9 @@ export const themes = {
     foods_theme: {
         menu_picture: "./assets/cards/foodstheme/foods.png",
         backside: "./public/assets/cards/foodstheme/backface/foodsbackfacecard.png",
+        player_blue: "./assets/cards/foodstheme/player_blue_foods_theme.svg",
+        player_orange: "./assets/cards/foodstheme/player_orange_foods_theme.svg",
+        draw_scale: "./assets/cards/foodstheme/scale_icon_foods_theme.svg",
         frontside_path: "./public/assets/cards/foodstheme/frontface/",
         frontside: [
             "foodbowlfrontface.png",
@@ -54,6 +60,9 @@ export const themes = {
     da_projects_theme: {
         menu_picture: "./assets/cards/daprojectstheme/daprojects.png",
         backside: "./public/assets/cards/daprojectstheme/backface/dabackfacecard.png",
+        player_blue: "./assets/cards/daprojectstheme/player_blue_da_projects_theme.svg",
+        player_orange: "./assets/cards/daprojectstheme/player_orange_da_projects_theme.svg",
+        draw_scale: "./assets/cards/daprojectstheme/scale_icon_da_projects_theme.svg",
         frontside_path: "./public/assets/cards/daprojectstheme/frontface/",
         frontside: [
             "dabackendfrontface.png",
@@ -79,6 +88,9 @@ export const themes = {
     code_vibes_theme: {
         menu_picture: "./assets/cards/codevibestheme/codemenu.png",
         backside: "./public/assets/cards/codevibestheme/backface/codebackfacecard.png",
+        player_blue: "./assets/cards/codevibestheme/player_blue_code_vibes_theme.svg",
+        player_orange: "./assets/cards/codevibestheme/player_orange_code_vibes_theme.svg",
+        draw_scale: "./assets/cards/codevibestheme/scale_icon_code_vibes_theme.svg",
         frontside_path: "./public/assets/cards/codevibestheme/frontface/",
         frontside: [
             "codeangularfrontface.png",
@@ -121,13 +133,4 @@ export const boardSizes = {
     "16_cards": 8,
     "24_cards": 12,
     "36_cards": 18
-};
-
-export const players = {
-    player_blue: {
-        color: "#3B82F6"
-    },
-    player_orange: {
-        color: "#F97316"
-    }
 };
